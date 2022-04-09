937f38fdbfc46ffc4d0b21476574f1efb2083798
Jan Dryk <me@uxsoft.cz>
2022-04-09 20:17:35 +0200

    add signalr

--- api/Negotiate.fs
+++ api/Negotiate.fs
@@ -1,13 +1,20 @@
 ﻿namespace Api
 
-open Microsoft.Azure.Functions.Worker
+open System
+open System.IO
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Azure.WebJobs
+open Microsoft.Azure.WebJobs.Extensions.Http
+open Microsoft.AspNetCore.Http
+open Microsoft.Azure.WebJobs.Extensions.SignalRService
+open Newtonsoft.Json
 open Microsoft.Extensions.Logging
 
-//module Negotiate =
+module Negotiate =
     
-//    [<Function("Negotiate")>]
-//    let run 
-//        ([<HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)>] req: HttpRequest) 
-//        ([<SignalRConnectionInfo(HubName = "serverless")>] connectionInfo: SignalRConnectionInfo)
-//        (log: ILogger) =
-//            connectionInfo
+    [<FunctionName("Negotiate")>]
+    let run 
+        ([<HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)>] req: HttpRequest) 
+        ([<SignalRConnectionInfo(HubName = "serverless")>] connectionInfo: SignalRConnectionInfo)
+        (log: ILogger) =
+            connectionInfo
