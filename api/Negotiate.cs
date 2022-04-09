2459e16ec6fe89e6b97949c73349b4ea060abca5
Jan Dryk <me@uxsoft.cz>
2022-04-09 20:49:02 +0200

    add signalr

---
+++ api/Negotiate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Api;
+
+public static class Negotiate
+{
+    [FunctionName("negotiate")]
+    public static SignalRConnectionInfo Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous)]HttpRequest req,
+        [SignalRConnectionInfo(HubName = "chat")]SignalRConnectionInfo connectionInfo)
+    {
+        return connectionInfo;
+    }
+}
